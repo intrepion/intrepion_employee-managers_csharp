7d6de87580740b54e79af6844304bff9f86c268d
Oliver Forral <intrepion@gmail.com>
2024-11-27 10:52:15 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"EmployeeManager\") }; { print }" Sco.SarTool.BusinessLogic/Grid/Admin/EmployeeManagerGrid/IEmployeeManagerFilters.cs

--- Sco.SarTool.BusinessLogic/Grid/Admin/EmployeeManagerGrid/IEmployeeManagerFilters.cs
+++ Sco.SarTool.BusinessLogic/Grid/Admin/EmployeeManagerGrid/IEmployeeManagerFilters.cs
@@ -1,8 +1,8 @@
-﻿namespace Sco.SarTool.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+﻿namespace Sco.SarTool.BusinessLogic.Grid.Admin.EmployeeManagerGrid;
 
-public interface IEntityNamePlaceholderFilters
+public interface IEmployeeManagerFilters
 {
-    EntityNamePlaceholderFilterColumns FilterColumn { get; set; }
+    EmployeeManagerFilterColumns FilterColumn { get; set; }
 
     bool Loading { get; set; }
 
@@ -12,5 +12,5 @@ public interface IEntityNamePlaceholderFilters
 
     bool SortAscending { get; set; }
 
-    EntityNamePlaceholderFilterColumns SortColumn { get; set; }
+    EmployeeManagerFilterColumns SortColumn { get; set; }
 }
