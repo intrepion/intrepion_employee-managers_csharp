232575553a364947725d98224066a03f40c7ec5e
Oliver Forral <intrepion@gmail.com>
2024-11-27 10:52:39 -0700

    gawk -i inplace "{ gsub(/\/\/ RecordPropertyCodePlaceholder/, \"public string Employee_Manager_NormalizedLastName { get; set; } = string.Empty;\n\ \ \ \ // RecordPropertyCodePlaceholder\") }; { print }" Sco.SarTool.BusinessLogic/Entities/Records/EmployeeManagerRecord.cs

--- Sco.SarTool.BusinessLogic/Entities/Records/EmployeeManagerRecord.cs
+++ Sco.SarTool.BusinessLogic/Entities/Records/EmployeeManagerRecord.cs
@@ -5,5 +5,6 @@ public class EmployeeManagerRecord
     public string Employee_Employee_NormalizedFirstName { get; set; } = string.Empty;
     public string Employee_Employee_NormalizedLastName { get; set; } = string.Empty;
     public string Employee_Manager_NormalizedFirstName { get; set; } = string.Empty;
+    public string Employee_Manager_NormalizedLastName { get; set; } = string.Empty;
     // RecordPropertyCodePlaceholder
 }
