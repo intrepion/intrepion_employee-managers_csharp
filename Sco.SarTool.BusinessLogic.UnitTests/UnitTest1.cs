707693a8c27e5e0055ef1c003d6ac1f10007caca
Oliver Forral <intrepion@gmail.com>
2024-11-27 10:43:39 -0700

    dotnet new nunit --name Sco.SarTool.BusinessLogic.UnitTests

---
+++ Sco.SarTool.BusinessLogic.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Sco.SarTool.BusinessLogic.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
