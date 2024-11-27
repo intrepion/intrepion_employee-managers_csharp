fda44a883a194f7774d8220eda897aeb2ff51a74
Oliver Forral <intrepion@gmail.com>
2024-11-27 10:52:08 -0700

    gawk -i inplace "{ gsub(/TableNamePlaceholder/, \"EmployeeManagers\") }; { print }" Sco.SarTool.BusinessLogic/Entities/Etcs/EmployeeManagerEtc.cs

--- Sco.SarTool.BusinessLogic/Entities/Etcs/EmployeeManagerEtc.cs
+++ Sco.SarTool.BusinessLogic/Entities/Etcs/EmployeeManagerEtc.cs
@@ -8,7 +8,7 @@ public class EmployeeManagerEtc : IEntityTypeConfiguration<EmployeeManager>
     public void Configure(EntityTypeBuilder<EmployeeManager> builder)
     {
         builder.HasOne(x => x.ApplicationUserUpdatedBy)
-            .WithMany(x => x.UpdatedTableNamePlaceholder)
+            .WithMany(x => x.UpdatedEmployeeManagers)
             .OnDelete(DeleteBehavior.Restrict);
 
         // EntityConfigurationCodePlaceholder
