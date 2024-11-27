1c6e5b77be1888579ffd8cd3e0cb0f7b6aa7d91b
Oliver Forral <intrepion@gmail.com>
2024-11-27 10:51:16 -0700

    gawk -i inplace "{ gsub(/\/\/ RecordPropertyCodePlaceholder/, \"public bool IsVerified { get; set; }\n\ \ \ \ // RecordPropertyCodePlaceholder\") }; { print }" Sco.SarTool.BusinessLogic/Entities/Records/EmployeeRecord.cs

--- Sco.SarTool.BusinessLogic/Entities/Records/EmployeeRecord.cs
+++ Sco.SarTool.BusinessLogic/Entities/Records/EmployeeRecord.cs
@@ -5,5 +5,6 @@ public class EmployeeRecord
     public string Email { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
     public bool IsTest { get; set; }
+    public bool IsVerified { get; set; }
     // RecordPropertyCodePlaceholder
 }
