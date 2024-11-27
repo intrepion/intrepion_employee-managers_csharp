2f9862fad0990970fd224b3beff90d1747a4af04
Oliver Forral <intrepion@gmail.com>
2024-11-27 10:52:30 -0700

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public Employee? Employee { get; set; }\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" Sco.SarTool.BusinessLogic/Entities/EmployeeManager.cs

--- Sco.SarTool.BusinessLogic/Entities/EmployeeManager.cs
+++ Sco.SarTool.BusinessLogic/Entities/EmployeeManager.cs
@@ -7,5 +7,6 @@ public class EmployeeManager
     public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
     public Guid Id { get; set; }
 
+    public Employee? Employee { get; set; }
     // ActualPropertyPlaceholder
 }
