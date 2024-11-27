9bbdf430a88fe33ec83df28bdb1b3d5cde92ffcb
Oliver Forral <intrepion@gmail.com>
2024-11-27 10:50:18 -0700

    gawk -i inplace "{ gsub(/\/\/ CompositeKeyCodePlaceholder/, \"\\\\&\\\\& x.NormalizedFirstName.Equals(employee.NormalizedFirstName)\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ // CompositeKeyCodePlaceholder\") }; { print }" Sco.SarTool.BusinessLogic/Entities/Importers/EmployeeImporter.cs

--- Sco.SarTool.BusinessLogic/Entities/Importers/EmployeeImporter.cs
+++ Sco.SarTool.BusinessLogic/Entities/Importers/EmployeeImporter.cs
@@ -63,6 +63,7 @@ public static class EmployeeImporter
 
                 var dbEmployee = await context.Employees.SingleOrDefaultAsync(
                     x => true
+                    && x.NormalizedFirstName.Equals(employee.NormalizedFirstName)
                     // CompositeKeyCodePlaceholder
                 );
 
