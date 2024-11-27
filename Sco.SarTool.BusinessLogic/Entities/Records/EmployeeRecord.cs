1069160d00ad5732fc021d7d234d35ae241f9f51
Oliver Forral <intrepion@gmail.com>
2024-11-27 10:51:06 -0700

    gawk -i inplace "{ gsub(/\/\/ RecordPropertyCodePlaceholder/, \"public bool IsTest { get; set; }\n\ \ \ \ // RecordPropertyCodePlaceholder\") }; { print }" Sco.SarTool.BusinessLogic/Entities/Records/EmployeeRecord.cs

--- Sco.SarTool.BusinessLogic/Entities/Records/EmployeeRecord.cs
+++ Sco.SarTool.BusinessLogic/Entities/Records/EmployeeRecord.cs
@@ -4,5 +4,6 @@ public class EmployeeRecord
 {
     public string Email { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
+    public bool IsTest { get; set; }
     // RecordPropertyCodePlaceholder
 }
