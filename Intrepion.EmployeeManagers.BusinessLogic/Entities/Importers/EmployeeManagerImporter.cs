05a4beee5786e9695510e98fc7d0253aaab1d321
Oliver Forral <intrepion@gmail.com>
2024-11-28 15:57:13 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"EmployeeManager\") }; { print }" Intrepion.EmployeeManagers.BusinessLogic/Entities/Importers/EmployeeManagerImporter.cs

--- Intrepion.EmployeeManagers.BusinessLogic/Entities/Importers/EmployeeManagerImporter.cs
+++ Intrepion.EmployeeManagers.BusinessLogic/Entities/Importers/EmployeeManagerImporter.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Importers;
 
-public static class EntityNamePlaceholderImporter
+public static class EmployeeManagerImporter
 {
     public static async Task ImportAsync(
        ApplicationDbContext context,
@@ -42,7 +42,7 @@ public static class EntityNamePlaceholderImporter
             Delimiter = "|",
         });
 
-        var records = csv.GetRecords<EntityNamePlaceholderRecord>();
+        var records = csv.GetRecords<EmployeeManagerRecord>();
 
         // EntityListCodePlaceholder
 
@@ -54,25 +54,25 @@ public static class EntityNamePlaceholderImporter
                 // NullCheckCodePlaceholder
             )
             {
-                var LowercaseNamePlaceholder = new EntityNamePlaceholder
+                var LowercaseNamePlaceholder = new EmployeeManager
                 {
                     ApplicationUserUpdatedBy = applicationUserUpdatedBy,
 
                     // NewEntityCodePlaceholder
                 };
 
-                var dbEntityNamePlaceholder = await context.TableNamePlaceholder.SingleOrDefaultAsync(
+                var dbEmployeeManager = await context.TableNamePlaceholder.SingleOrDefaultAsync(
                     x => true
                     // CompositeKeyCodePlaceholder
                 );
 
-                if (dbEntityNamePlaceholder is null)
+                if (dbEmployeeManager is null)
                 {
                     await context.TableNamePlaceholder.AddAsync(LowercaseNamePlaceholder);
                 }
                 else
                 {
-                    dbEntityNamePlaceholder.ApplicationUserUpdatedBy = applicationUserUpdatedBy;
+                    dbEmployeeManager.ApplicationUserUpdatedBy = applicationUserUpdatedBy;
 
                     // ExistingEntityCodePlaceholder
                 }
