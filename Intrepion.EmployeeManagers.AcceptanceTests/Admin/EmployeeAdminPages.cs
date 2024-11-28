3a0b6282763546373adc8d9cccf2edf05799838a
Oliver Forral <intrepion@gmail.com>
2024-11-28 00:44:57 -0700

    gawk -i inplace "{ gsub(/\/\/ CreatePropertyCodePlaceholder/, \"await Page.GetByLabel(\\\"First Name:\\\", new() { Exact = true }).FillAsync(\\\"aFirstName\\\" + aRandomString);\n\ \ \ \ \ \ \ \ // CreatePropertyCodePlaceholder\") }; { print }" Intrepion.EmployeeManagers.AcceptanceTests/Admin/EmployeeAdminPages.cs

--- Intrepion.EmployeeManagers.AcceptanceTests/Admin/EmployeeAdminPages.cs
+++ Intrepion.EmployeeManagers.AcceptanceTests/Admin/EmployeeAdminPages.cs
@@ -19,6 +19,7 @@ public partial class EmployeeAdminPages : PageTest
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Employee Add");
 
+        await Page.GetByLabel("First Name:", new() { Exact = true }).FillAsync("aFirstName" + aRandomString);
         // CreatePropertyCodePlaceholder
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
