00b5a8489b11a003c4c780c05d055f24e6d1125a
Oliver Forral <intrepion@gmail.com>
2024-11-28 00:47:03 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.EmployeeManagers\") }; { print }" Intrepion.EmployeeManagers.AcceptanceTests/Admin/*

--- Intrepion.EmployeeManagers.AcceptanceTests/Admin/EmployeeManagerAdminPages.cs
+++ Intrepion.EmployeeManagers.AcceptanceTests/Admin/EmployeeManagerAdminPages.cs
@@ -2,7 +2,7 @@
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 
-namespace ApplicationNamePlaceholder.AcceptanceTests.Admin;
+namespace Intrepion.EmployeeManagers.AcceptanceTests.Admin;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
@@ -48,8 +48,8 @@ public partial class EmployeeManagerAdminPages : PageTest
 
         await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Log in");
-        await Page.GetByTestId("loginEmail").FillAsync("Admin1@ApplicationNamePlaceholder.com");
-        await Page.GetByTestId("loginPassword").FillAsync("Admin1@ApplicationNamePlaceholder.com");
+        await Page.GetByTestId("loginEmail").FillAsync("Admin1@Intrepion.EmployeeManagers.com");
+        await Page.GetByTestId("loginPassword").FillAsync("Admin1@Intrepion.EmployeeManagers.com");
         await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
     }
 
