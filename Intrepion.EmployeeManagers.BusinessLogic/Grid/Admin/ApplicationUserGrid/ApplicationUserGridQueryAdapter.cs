138f4050566b9da910e16c254cb89e7c9f1683cb
Oliver Forral <intrepion@gmail.com>
2024-11-28 15:58:44 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.EmployeeManagers\") }; { print }" Intrepion.EmployeeManagers.BusinessLogic/Grid/Admin/ApplicationUserGrid/*

--- Intrepion.EmployeeManagers.BusinessLogic/Grid/Admin/ApplicationUserGrid/ApplicationUserGridQueryAdapter.cs
+++ Intrepion.EmployeeManagers.BusinessLogic/Grid/Admin/ApplicationUserGrid/ApplicationUserGridQueryAdapter.cs
@@ -1,9 +1,9 @@
 using System.Diagnostics;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
-using ApplicationNamePlaceholder.BusinessLogic.Entities;
+using Intrepion.EmployeeManagers.BusinessLogic.Entities;
 
-namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.ApplicationUserGrid;
+namespace Intrepion.EmployeeManagers.BusinessLogic.Grid.Admin.ApplicationUserGrid;
 
 public class ApplicationUserGridQueryAdapter
 {
