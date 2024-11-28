6ed8ce5a7fc3c2d24d57acf609f5abab5d81d507
Oliver Forral <intrepion@gmail.com>
2024-11-28 15:54:26 -0700

    gawk -i inplace "{ gsub(/TableNamePlaceholder/, \"Employees\") }; { print }" Intrepion.EmployeeManagers.BusinessLogic/Entities/Etcs/EmployeeEtc.cs

--- Intrepion.EmployeeManagers.BusinessLogic/Entities/Etcs/EmployeeEtc.cs
+++ Intrepion.EmployeeManagers.BusinessLogic/Entities/Etcs/EmployeeEtc.cs
@@ -8,7 +8,7 @@ public class EmployeeEtc : IEntityTypeConfiguration<Employee>
     public void Configure(EntityTypeBuilder<Employee> builder)
     {
         builder.HasOne(x => x.ApplicationUserUpdatedBy)
-            .WithMany(x => x.UpdatedTableNamePlaceholder)
+            .WithMany(x => x.UpdatedEmployees)
             .OnDelete(DeleteBehavior.Restrict);
 
         // EntityConfigurationCodePlaceholder
