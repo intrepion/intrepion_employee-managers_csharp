0e500a74c0ad8e61bcf11f54ba132eb78ffe051f
Oliver Forral <intrepion@gmail.com>
2024-11-28 15:58:10 -0700

    gawk -i inplace "{ gsub(/\/\/ ManyToOneCodePlaceholder/, \"var manager = managerList.FirstOrDefault(x =>\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ true\n\ \ \ \ \ \ \ \ \ \ \ \ // ManyToOneCodePlaceholder\") }; { print }" Intrepion.EmployeeManagers.BusinessLogic/Entities/Importers/EmployeeManagerImporter.cs

--- Intrepion.EmployeeManagers.BusinessLogic/Entities/Importers/EmployeeManagerImporter.cs
+++ Intrepion.EmployeeManagers.BusinessLogic/Entities/Importers/EmployeeManagerImporter.cs
@@ -56,6 +56,8 @@ public static class EmployeeManagerImporter
                 && x.NormalizedLastName.Equals(record.Employee_NormalizedLastName)
             );
 
+            var manager = managerList.FirstOrDefault(x =>
+                true
             // ManyToOneCodePlaceholder
 
             if (true
