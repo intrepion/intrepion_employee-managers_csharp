16a2381b7b87683de1004b41845f80550bf7f4cc
Oliver Forral <intrepion@gmail.com>
2024-11-28 00:46:18 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"EmployeeManager\") }; { print }" Intrepion.EmployeeManagers.BusinessLogic/Entities/Etcs/EmployeeManagerEtc.cs

--- Intrepion.EmployeeManagers.BusinessLogic/Entities/Etcs/EmployeeManagerEtc.cs
+++ Intrepion.EmployeeManagers.BusinessLogic/Entities/Etcs/EmployeeManagerEtc.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
 
-public class EntityNamePlaceholderEtc : IEntityTypeConfiguration<EntityNamePlaceholder>
+public class EmployeeManagerEtc : IEntityTypeConfiguration<EmployeeManager>
 {
-    public void Configure(EntityTypeBuilder<EntityNamePlaceholder> builder)
+    public void Configure(EntityTypeBuilder<EmployeeManager> builder)
     {
         builder.HasOne(x => x.ApplicationUserUpdatedBy)
             .WithMany(x => x.UpdatedTableNamePlaceholder)
