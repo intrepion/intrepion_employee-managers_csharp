27f1a5c4d43513bdbf6fbb6693922ec15ed8d643
Oliver Forral <intrepion@gmail.com>
2024-11-28 15:56:00 -0700

    gawk -i inplace "{ gsub(/\/\/ RecordPropertyCodePlaceholder/, \"public bool IsVerified { get; set; }\n\ \ \ \ // RecordPropertyCodePlaceholder\") }; { print }" Intrepion.EmployeeManagers.BusinessLogic/Entities/Records/EmployeeRecord.cs

--- Intrepion.EmployeeManagers.BusinessLogic/Entities/Records/EmployeeRecord.cs
+++ Intrepion.EmployeeManagers.BusinessLogic/Entities/Records/EmployeeRecord.cs
@@ -4,5 +4,6 @@ public class EmployeeRecord
 {
     public string FirstName { get; set; } = string.Empty;
     public bool IsTest { get; set; }
+    public bool IsVerified { get; set; }
     // RecordPropertyCodePlaceholder
 }
