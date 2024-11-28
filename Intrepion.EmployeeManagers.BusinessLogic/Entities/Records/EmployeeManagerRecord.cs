7fab88ce82536dcfecc0f196e57bee1aeda5972f
Oliver Forral <intrepion@gmail.com>
2024-11-28 15:58:16 -0700

    gawk -i inplace "{ gsub(/\/\/ RecordPropertyCodePlaceholder/, \"public string Manager_NormalizedLastName { get; set; } = string.Empty;\n\ \ \ \ // RecordPropertyCodePlaceholder\") }; { print }" Intrepion.EmployeeManagers.BusinessLogic/Entities/Records/EmployeeManagerRecord.cs

--- Intrepion.EmployeeManagers.BusinessLogic/Entities/Records/EmployeeManagerRecord.cs
+++ Intrepion.EmployeeManagers.BusinessLogic/Entities/Records/EmployeeManagerRecord.cs
@@ -5,5 +5,6 @@ public class EmployeeManagerRecord
     public string Employee_NormalizedFirstName { get; set; } = string.Empty;
     public string Employee_NormalizedLastName { get; set; } = string.Empty;
     public string Manager_NormalizedFirstName { get; set; } = string.Empty;
+    public string Manager_NormalizedLastName { get; set; } = string.Empty;
     // RecordPropertyCodePlaceholder
 }
