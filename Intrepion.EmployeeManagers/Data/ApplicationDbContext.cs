8b62b3b5976b56663b93ff7974a82a09bf62a9c8
Oliver Forral <intrepion@gmail.com>
2025-03-07 00:41:51 -0700

    dotnet format

---
+++ Intrepion.EmployeeManagers/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Intrepion.EmployeeManagers.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+}
