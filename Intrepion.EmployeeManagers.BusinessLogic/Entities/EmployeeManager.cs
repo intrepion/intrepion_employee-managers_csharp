b76365aa3a7ee0e231c760d331119c1fe593525d
Oliver Forral <intrepion@gmail.com>
2025-03-10 22:16:34 -0600

    dotnet ef --project Intrepion.EmployeeManagers.BusinessLogic --startup-project Intrepion.EmployeeManagers migrations add Initial

---
+++ Intrepion.EmployeeManagers.BusinessLogic/Entities/EmployeeManager.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Intrepion.EmployeeManagers.BusinessLogic.Entities;
+
+public class EmployeeManager
+{
+    public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
+    public Guid Id { get; set; }
+    public DateTime UpdateDateTime { get; set; }
+
+    public Employee? Employee { get; set; }
+    public Employee? Manager { get; set; }
+    // ActualPropertyPlaceholder
+}
---
+++ Intrepion.EmployeeManagers.BusinessLogic/Entities/Etcs/EmployeeManagerEtc.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Intrepion.EmployeeManagers.BusinessLogic.Entities.Configuration;
+
+public class EmployeeManagerEtc : IEntityTypeConfiguration<EmployeeManager>
+{
+    public void Configure(EntityTypeBuilder<EmployeeManager> builder)
+    {
+        builder.HasOne(x => x.ApplicationUserUpdatedBy)
+            .WithMany(x => x.UpdatedEmployeeManagers)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.Employee)
+            .WithMany(x => x.EmployeeManagers)
+            .OnDelete(DeleteBehavior.Restrict);
+        builder.HasOne(x => x.Manager)
+            .WithMany(x => x.ManagerEmployees)
+            .OnDelete(DeleteBehavior.Restrict);
+        // EntityConfigurationCodePlaceholder
+    }
+}
